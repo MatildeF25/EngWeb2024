346398f66b4adf162d6090bbb2cc3149497c7eaa
MatildeF25 <a95319@alunos.uminho.pt>
2024-02-12 14:38:07 +0000

    tpc1 css

--- TPC1/MapaRuas-materialBase/geraPag.py
+++ TPC1/MapaRuas-materialBase/geraPag.py
@@ -26,9 +26,19 @@ def extract_description(file):
 
     return description
 
+
+def get_fotos_atuais(numero):
+    fotos = []
+    numero_f = numero + "-"
+    for file in os.listdir("atual"):
+        if file.startswith(numero_f):
+            fotos.append(file)
+    return fotos
+
 for file in os.listdir("texto"):
     numero = ""  # Initialize numero
     nome = ""  # Initialize nome
+    atuais = []
     # criar um tuplo imagem, legenda
     imagem = ""
     legenda = ""
@@ -47,6 +57,7 @@ for file in os.listdir("texto"):
                 for meta in child:
                     if meta.tag == "número":
                         numero = meta.text
+                        atuais = get_fotos_atuais(numero)
                     if meta.tag == "nome":
                         nome = meta.text
             if child.tag == "corpo":
@@ -68,26 +79,31 @@ for file in os.listdir("texto"):
                                         dono = casa.text
                                     if casa.tag == "foro":
                                         custo = casa.text
+                                    d = ""
                                     if casa.tag == "desc":
-                                        para = casa.find('para')
-                                        if para is not None:
-                                            d = ""
+                                        para_t = casa.find('para')
+                                        if para_t is not None:
                                             para = ""
-                                            for text in casa.itertext():
+                                            for text in para_t.itertext():
+                                                #print(para)
                                                 para += text
-                                            for lugar in casa.findall('lugar'):
+                                            for lugar in para_t.findall('lugar'):
                                                 para = para.replace(lugar.text, f"<b>{lugar.text}</b>")
-                                            for data in casa.findall('data'):
-                                                para = para.replace(data.text, f"<b>{data.text}</b>")
-                                            for entidade in casa.findall('entidade'):
+                                            for data in para_t.findall('data'):
+                                                para = para.replace(data.text, f"<i>{data.text}</i>")
+                                            for entidade in para_t.findall('entidade'):
                                                 para = para.replace(entidade.text, f"<b>{entidade.text}</b>")
                                             d = "<p>" + para + "</p>\n"
                                 casas.append((n, dono, custo, d))
                             
-        bd.append((file[:-4], numero, nome, figura, description,casas))
+        bd.append((file[:-4], numero, nome, figura, description,casas, atuais))
 
 
 bd.sort(key=lambda x: int(x[1]))
+# print values for key 1
+valor = bd[0]
+
+
 
 
 
@@ -100,6 +116,7 @@ for e in bd:
     <title>{e[2]}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="w3.css">
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8"/>
 </head>
 <body>
@@ -109,10 +126,27 @@ for e in bd:
         </header>
 
         <div class="w3-container w3-center">
-            <h3>Vistas</h3>
+            <h3><b>Vistas atuais</b></h3>
         </div>
         <div class="w3-row-padding w3-margin-top">
     """
+
+    for foto in e[6]:
+        poshtml += f"""
+            <div class="w3-third">
+                <div class="w3-card">
+                    <img src="../MapaRuas-materialBase/atual/{foto}" style="width:100%">
+                </div>
+            </div>
+        """
+    poshtml += f"""
+        </div>
+        <div class="w3-container w3-center">
+            <h3><b>Vistas antigas</b></h3>
+        
+            <div class="w3-row-padding w3-margin-top">
+        """
+
     
     i = 1
     for fig in e[3]:
@@ -145,14 +179,18 @@ for e in bd:
 
     
     poshtml += f"""
+            </div>
+        </div>    
+        <br></br>
+        <div class="example">
+            <div class="w3-container w3-center">
+            <h3><b>Descrição</b></h3>
+            </div>
+                {e[4]}
         </div>
+        
         <div class="w3-container w3-center">
-            <h3>Descrição</h3>
-        </div>
-
-        {e[4]}
-        <div class="w3-container w3-center">
-              <h3>Casas</h3>
+              <h3><b>Casas</b></h3>
             </div>
 
             <table class="w3-table-all">
