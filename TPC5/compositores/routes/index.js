7a0588c21845b4523148020e27c7e01c67d7f0f1
MatildeF25 <a95319@alunos.uminho.pt>
2024-03-12 13:32:39 +0000

    TPC5

--- TPC5/compositores/routes/index.js
+++ TPC5/compositores/routes/index.js
@@ -19,6 +19,48 @@ router.get('/compositores', function(req, res, next) {
     });
 });
 
+router.get('/compositores/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  res.render('registoCompositor', { data: d, title: 'Registo de Compositor' });
+});
+
+
+router.get('/compositores/:idCompositor', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get('http://localhost:3000/compositores/' + req.params.idCompositor)
+    .then(resposta => {
+      res.render('compositor', { compositor: resposta.data, data: d, title: 'Compositor' + resposta.data.nome });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na listagem do compositor' });
+    });
+});
+
+router.get('/compositores/edit/:idCompositor', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get('http://localhost:3000/compositores/' + req.params.idCompositor)
+    .then(resposta => {
+      res.render('editarCompositor', { c: resposta.data, data: d, title: 'Editar Compositor' + resposta.data.nome });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na listagem do compositor' });
+    });
+});
+
+
+router.get('/compositores/delete/:idCompositor', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.delete('http://localhost:3000/compositores/' + req.params.idCompositor)
+    .then(resposta => {
+      res.render('compositor', {compositor: resposta.data, data: d, title: 'Confirmação de Eliminação de Compositor' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na eliminação do compositor' });
+    });
+});
+
+
+
 router.get('/periodo', function(req, res) {
   var d = new Date().toISOString().substring(0, 16)
   axios.get('http://localhost:3000/periodo')
@@ -28,7 +70,93 @@ router.get('/periodo', function(req, res) {
     .catch(erro => {
       res.render('error', { error: erro, message: 'Erro na listagem de periodos' });
     });
-})
+});
+
+router.get('/periodo/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  res.render('registoPeriodo', { data: d, title: 'Registo de Periodo' });
+});
+
+
+router.get('/periodo/:idPeriodo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get('http://localhost:3000/periodo/' + req.params.idPeriodo)
+    .then(resposta => {
+      res.render('periodo', { periodo: resposta.data, data: d, title: 'Periodo' + resposta.data.nome });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na listagem do periodo' });
+    });
+});
+
+
+router.get('/periodo/edit/:idPeriodo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get('http://localhost:3000/periodo/' + req.params.idPeriodo)
+    .then(resposta => {
+      res.render('editarPeriodo', { p: resposta.data, data: d, title: 'Editar Periodo' + resposta.data.nome });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na listagem do periodo' });
+    });
+});
+
+router.get('/periodo/delete/:idPeriodo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.delete('http://localhost:3000/periodo/' + req.params.idPeriodo)
+    .then(resposta => {
+      res.render('periodo', {periodo: resposta.data, data: d, title: 'Confirmação de Eliminação de Periodo' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na eliminação do periodo' });
+    });
+});
+
+
+router.post('/compositores/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.post('http://localhost:3000/compositores', req.body)
+    .then(resposta => {
+      res.render('compositor', {compositor: req.body, data: d, title: 'Confirmação de Registo de Compositor' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro no registo do compositor' });
+    });
+});
+
+
+router.post('/periodo/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.post('http://localhost:3000/periodo', req.body)
+    .then(resposta => {
+      res.render('periodo', {periodo: req.body, data: d, title: 'Confirmação de Registo de Periodo' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro no registo do periodo' });
+    });
+});
+
+router.post('/compositores/edit/:idCompositor', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.put('http://localhost:3000/compositores/' + req.params.idCompositor, req.body)
+    .then(resposta => {
+      res.render('compositor', {compositor: req.body, data: d, title: 'Confirmação de Edição de Compositor' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na edição do compositor' });
+    });
+});
+
+router.post('/periodo/edit/:idPeriodo', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.put('http://localhost:3000/periodo/' + req.params.idPeriodo, req.body)
+    .then(resposta => {
+      res.render('periodo', {periodo: req.body, data: d, title: 'Confirmação de Edição de Periodo' });
+    })
+    .catch(erro => {
+      res.render('error', { error: erro, message: 'Erro na edição do periodo' });
+    });
+});
 
 
 module.exports = router;
